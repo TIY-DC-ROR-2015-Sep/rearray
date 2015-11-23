b54b46cba4d87c57b534a52f698cc3a617ac61a0
James Dabbs <jamesdabbs@gmail.com>
2015-11-23 14:53:44 -0500

    Passing first test

--- rearray.rb
+++ rearray.rb
@@ -1,4 +1,28 @@
-#Rearray = Array
+class Node
+  def initialize value
+    @value = value
+  end
+
+  def value
+    @value
+  end
+end
 
 class Rearray
+  def initialize
+    @start = nil
+  end
+
+  def push thing
+    node = Node.new thing
+    if @start
+      i_dont_know
+    else
+      @start = node
+    end
+  end
+
+  def first
+    @start.value
+  end
 end
