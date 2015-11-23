d5529b392333d2d87bbca4b96a5e7fe3656acf0a
James Dabbs <jamesdabbs@gmail.com>
2015-11-23 15:02:50 -0500

    Add size method

--- rearray.rb
+++ rearray.rb
@@ -1,25 +1,37 @@
 class Node
-  def initialize value
-    @value = value
-  end
+  attr_accessor :next, :value
 
-  def value
-    @value
+  def initialize value
+    self.value = value
   end
 end
 
 class Rearray
   def initialize
     @start = nil
+    @last  = nil
   end
 
   def push thing
     node = Node.new thing
     if @start
-      i_dont_know
+      @last.next = node
+      @last = node
     else
       @start = node
+      @last  = node
     end
+  end
+
+  def size
+    return 0 unless @start
+    curr  = @start
+    count = 1
+    while curr != @last
+      curr = curr.next
+      count += 1
+    end
+    count
   end
 
   def first
